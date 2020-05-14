ecd1ebb496fe2a54741a730b4c0e4b0cc55e2b26
Eric Pyle <eric_pyle@sil.org>
2020-05-14 16:00:11 -0500

    add transformToVerses

--- src/loadBibles.js
+++ src/loadBibles.js
@@ -1,7 +1,20 @@
-import fs from 'fs';
+import traverse from 'traverse';
 // import bookNames from './Bible-kjv/Books.json';
 // import * as booksData from './Bible-kjv/';
 // import loadJsonFile from 'load-json-file';
+
+function transformToVerses(acc, bookDataNode) {
+  if (this.key === "verses") {
+    const chapter = this.parent.node["chapter"];
+    const book = this.parent.parent.parent.node["name"];
+    const versesWithContext = bookDataNode.map((verse) => [
+      { verse: verse.verse, text: verse.text, chapter, book },
+    ]);
+    return [...acc, ...versesWithContext];
+  }
+  return acc;
+}
+
 
 export async function loadBookData() {
     const response = await fetch('https://raw.githubusercontent.com/aruljohn/Bible-kjv/master/Books.json');
@@ -12,6 +25,7 @@ export async function loadBookData() {
             const bookResponse = await fetch(`https://raw.githubusercontent.com/aruljohn/Bible-kjv/master/${strippedBookName}.json`);
             if (bookResponse.ok) {
                 const bookData = bookResponse.json();
+                traverse(bookData).reduce(transformToVerses, {})
             }
             return acc;
         }, []);
