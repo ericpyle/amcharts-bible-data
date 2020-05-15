0c30e82ab61b2a3265ca76dde86c2233f69b16b2
Eric Pyle <eric_pyle@sil.org>
2020-05-14 23:06:35 -0500

    convert to xAxis ValueAxis

--- src/amchart.js
+++ src/amchart.js
@@ -2,6 +2,7 @@
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+// import * as versesData from './kjv-verses.json';
 
 /* Chart code */
 // Themes begin
@@ -13,33 +14,33 @@ chart.paddingRight = 20;
 
 let data = [];
 let visits = 10;
-for (var i = 1; i < 50000; i++) {
+for (let i = 1; i < 1000; i++) {
   visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-  data.push({ date: new Date(2018, 0, i), value: visits });
+  data.push({ x: i, value: visits });
 }
 
 chart.data = data;
 
-let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-dateAxis.renderer.grid.template.location = 0;
-dateAxis.minZoomCount = 5;
+let xAxis = chart.xAxes.push(new am4charts.ValueAxis());
+xAxis.renderer.grid.template.location = 0;
+xAxis.minZoomCount = 5;
 
 
 // this makes the data to be grouped
-dateAxis.groupData = true;
-dateAxis.groupCount = 500;
+xAxis.groupData = true;
+xAxis.groupCount = 500;
 
 let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
 let series = chart.series.push(new am4charts.LineSeries());
-series.dataFields.dateX = "date";
+series.dataFields.valueX = "x";
 series.dataFields.valueY = "value";
 series.tooltipText = "{valueY}";
 series.tooltip.pointerOrientation = "vertical";
 series.tooltip.background.fillOpacity = 0.5;
 
 chart.cursor = new am4charts.XYCursor();
-chart.cursor.xAxis = dateAxis;
+chart.cursor.xAxis = xAxis;
 
 let scrollbarX = new am4core.Scrollbar();
 scrollbarX.marginBottom = 20;
