75d175b0d5f634b7d908d0b7a402347efbf34991
Eric Pyle <eric_pyle@sil.org>
2020-05-14 21:32:06 -0500

    fix allVersesData

--- src/loadBibles.js
+++ src/loadBibles.js
@@ -7,9 +7,9 @@ function transformToVerses(acc, bookDataNode) {
   if (this.key === "verses") {
     const chapter = this.parent.node["chapter"];
     const book = this.parent.parent.parent.node["book"];
-    const versesWithContext = bookDataNode.map((verse) => [
-      { verse: verse.verse, text: verse.text, chapter, book },
-    ]);
+    const versesWithContext = bookDataNode.map((verse) =>
+      ({ verse: verse.verse, text: verse.text, chapter, book })
+    );
     return [...acc, ...versesWithContext];
   }
   return acc;
@@ -18,20 +18,21 @@ function transformToVerses(acc, bookDataNode) {
 
 export async function loadBookData() {
     const response = await fetch('https://raw.githubusercontent.com/aruljohn/Bible-kjv/master/Books.json');
-    if (response.ok) {
-        const bookNames = await response.json();
-        bookNames.reduce(async (acc, bookName) => {
-            const strippedBookName = bookName.replace(/ /gi, '');
-            const bookResponse = await fetch(`https://raw.githubusercontent.com/aruljohn/Bible-kjv/master/${strippedBookName}.json`);
-            if (bookResponse.ok) {
-                const bookData = bookResponse.json();
-                traverse(bookData).reduce(transformToVerses, {})
-            }
-            return acc;
-        }, []);
-        return bookNames;
-    }
-    return [];
+    if (!response.ok)
+        return [];
+    const bookNames = await response.json();
+    const eachBookVersesData = await Promise.all(bookNames.map(async (bookName) => {
+        const strippedBookName = bookName.replace(/ /gi, '');
+        const bookResponse = await fetch(`https://raw.githubusercontent.com/aruljohn/Bible-kjv/master/${strippedBookName}.json`);
+        if (!bookResponse.ok)
+            return [{book: bookName}];
+        const bookData = await bookResponse.json();
+        const versesInBook = traverse(bookData).reduce(transformToVerses, []);
+        return versesInBook;
+    }));
+    const allVersesData = eachBookVersesData.reduce((acc, versesInBook)=>acc.concat(versesInBook), []);
+    console.log({ allVersesData });
+    return allVersesData;
 }
 
 export default { loadBookData };
